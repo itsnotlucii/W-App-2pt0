ba2a61a97acf99075074358342332a81e13db40e
itsnotlucii <91422427+itsnotlucii@users.noreply.github.com>
2021-12-05 18:48:08 +1100

    Committing After Forgetting to Commit

---
+++ src/app.js
@@ -0,0 +1,163 @@
+let now = new Date();
+
+let p = document.querySelector("p#date");
+let date = now.getDate();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let year = now.getFullYear();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+p.innerHTML = `${day}, ${date} ${month} ${year} </br><small>Last Updated: ${hours}:${minutes}</small>`;
+
+function searchCity(city) {
+  let apiKey = "c83467d2813a5ff97677bad9626889da";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showCurrentWeather);
+}
+
+function searchSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", searchSubmit);
+
+function showCurrentWeather(response) {
+  celsiusTemp = response.data.main.temp;
+
+  document.querySelector("h2").innerHTML = `${response.data.name}`;
+  document.querySelector("h1").innerHTML = Math.round(celsiusTemp);
+  document.querySelector(
+    "h3"
+  ).innerHTML = `${response.data.weather[0].description}`;
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `<i class="fas fa-tint"></i> Humidity: ${response.data.main.humidity}%`;
+  document.querySelector(
+    "#wind"
+  ).innerHTML = `<i class="fas fa-wind"></i> Wind: ${Math.round(
+    response.data.wind.speed
+  )}km/h`;
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  getForecast(response.data.coord);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function getForecast(coordinates) {
+  let apiKey = "c83467d2813a5ff97677bad9626889da";
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(showForecast);
+}
+
+function showForecast(response) {
+  let forecastDaily = response.data.daily;
+  let forecast = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+
+  forecastDaily.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+     
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="40"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max">${Math.round(
+            forecastDay.temp.max
+          )}°</span>
+          <span class="weather-forecast-temperature-min">${Math.round(
+            forecastDay.temp.min
+          )}°</span>
+        </div>
+      </div>`;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecast.innerHTML = forecastHTML;
+}
+
+function getCurrentPosition(position) {
+  let apiKey = "c83467d2813a5ff97677bad9626889da";
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(showCurrentWeather);
+}
+
+function searchCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getCurrentPosition);
+}
+
+function wallpaper() {
+  let hour = now.getHours();
+  console.log(hour);
+  if (hours < 12) {
+    document.querySelector(".card").style.backgroundImage =
+      "url(././src/images/AM.jpeg)";
+  } else if (hours < 17) {
+    document.querySelector(".card").style.backgroundImage =
+      "url(././src/images/Mid.jpeg)";
+  } else {
+    document.querySelector(".card").style.backgroundImage =
+      "url(././src/images/PM.jpeg)";
+  }
+}
+wallpaper();
+
+let celsiusTemp = null;
+
+let button = document.querySelector("button");
+button.addEventListener("click", searchCurrentPosition);
+
+searchCity("Milan");
+showForecast();
