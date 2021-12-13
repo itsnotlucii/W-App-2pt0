31d3f2ff01ef293a4213e6cc30f57f26a744f4c7
itsnotlucii <91422427+itsnotlucii@users.noreply.github.com>
2021-12-13 22:23:55 +1100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -133,10 +133,10 @@ let celsiusTemperature = null;
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
+search("Sydney");
+
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
-search("Sydney");
